a7321b70a7f8d6b8d0067f8b62c84ce55dfd8ceb
박승권 <yevgnenll@gmail.com>
2020-11-25 20:53:29 +0900

    first commit

---
+++ src/test/java/me/yevgnenll/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yevgnenll.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

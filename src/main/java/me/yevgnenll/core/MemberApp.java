0f73d1b3ac707e1851435ca4ad1ae967d28f46e5
박승권 <yevgnenll@gmail.com>
2020-12-14 03:13:29 +0900

    스프링을 적용하여 코드를 개선한다.

--- src/main/java/me/yevgnenll/core/MemberApp.java
+++ src/main/java/me/yevgnenll/core/MemberApp.java
@@ -1,5 +1,8 @@
 package me.yevgnenll.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import me.yevgnenll.core.member.Grade;
 import me.yevgnenll.core.member.Member;
 import me.yevgnenll.core.service.MemberService;
@@ -7,8 +10,13 @@ import me.yevgnenll.core.service.MemberService;
 public class MemberApp {
 
   public static void main(String[] args) {
-    AppConfig config = new AppConfig();
-    MemberService memberService = config.memberService();
+
+    // spring container
+    // annotation 기반으로 정의한다
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    // config 에서 memberService() 를 가져와야 한다
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
     Member member = new Member(1L, "memberA", Grade.VIP);
     memberService.join(member);

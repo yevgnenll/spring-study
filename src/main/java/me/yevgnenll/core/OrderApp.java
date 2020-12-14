b029b215d82af2c7e53b3a65ef5a6d20bcc69d5c
박승권 <yevgnenll@gmail.com>
2020-12-14 13:32:37 +0900

    Spring container 를 이용해 Bean 을 가져온다.
    객체의 생성과 제어를 직접 하지 않고, Spring 에 맡겼다.

--- src/main/java/me/yevgnenll/core/OrderApp.java
+++ src/main/java/me/yevgnenll/core/OrderApp.java
@@ -1,5 +1,8 @@
 package me.yevgnenll.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import me.yevgnenll.core.member.Grade;
 import me.yevgnenll.core.member.Member;
 import me.yevgnenll.core.order.Order;
@@ -8,9 +11,10 @@ import me.yevgnenll.core.service.OrderService;
 
 public class OrderApp {
   public static void main(String[] args) {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
+
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = ac.getBean("memberService", MemberService.class);
+    OrderService orderService = ac.getBean("orderService", OrderService.class);
 
     Long memberId = 1L;
     Member member = new Member(memberId, "memberA", Grade.VIP);

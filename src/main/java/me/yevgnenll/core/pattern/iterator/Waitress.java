255b9d65ab89584534e6a0f2d55eb073c51261ea
박승권 <yevgnenll@gmail.com>
2021-01-26 15:27:57 +0900

    waitree 정의

---
+++ src/main/java/me/yevgnenll/core/pattern/iterator/Waitress.java
@@ -0,0 +1,31 @@
+package me.yevgnenll.core.pattern.iterator;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Iterator;
+
+@AllArgsConstructor
+public class Waitress {
+    private PancakeHouseMenu pancakeHouseMenu;
+    private DinerMenu dinerMenu;
+
+    public void printMenu() {
+        Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+        Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+
+        System.out.println("MENU\n----\nBREAKFAST");
+        printMenu(pancakeIterator);
+        System.out.println("\nLUNCH");
+        printMenu(dinerIterator);
+    }
+
+    private void printMenu(Iterator iterator) {
+        while (iterator.hasNext()) {
+            MenuItem menuItem = (MenuItem) iterator.next();
+            System.out.print(menuItem.getName() + ", ");
+            System.out.print(menuItem.getPrice() + " -- ");
+            System.out.println(menuItem.getDescription());
+        }
+    }
+
+}

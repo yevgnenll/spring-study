f536d2323c1fab8ff1b678c9c6810e1f6afa92e1
박승권 <yevgnenll@gmail.com>
2021-01-26 17:40:44 +0900

    MenuComponent 정의 및 구현

--- src/main/java/me/yevgnenll/core/pattern/iterator/MenuItem.java
+++ src/main/java/me/yevgnenll/core/pattern/iterator/MenuItem.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class MenuItem {
+public class MenuItem extends MenuComponent {
 
     private final String name;
     private final String description;
@@ -15,5 +15,14 @@ public class MenuItem {
     @Override
     public String toString() {
         return (name + ", $" + price + "\n   " + description);
+    }
+
+    public void print() {
+      System.out.print("  " + getName());
+      if (isVegetarian()) {
+        System.out.print("(v)");
+      }
+      System.out.println(", " + getPrice());
+      System.out.println("     -- " + getDescription());
     }
 }

6a09306ad4c8113b934707d5e5ced6fcaa501f51
박승권 <yevgnenll@gmail.com>
2021-01-26 15:29:08 +0900

    indent 수정

--- src/main/java/me/yevgnenll/core/pattern/iterator/Waitress.java
+++ src/main/java/me/yevgnenll/core/pattern/iterator/Waitress.java
@@ -10,22 +10,23 @@ public class Waitress {
     private DinerMenu dinerMenu;
 
     public void printMenu() {
-        Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
-        Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
 
-        System.out.println("MENU\n----\nBREAKFAST");
-        printMenu(pancakeIterator);
-        System.out.println("\nLUNCH");
-        printMenu(dinerIterator);
+      Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+      Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+
+      System.out.println("MENU\n----\nBREAKFAST");
+      printMenu(pancakeIterator);
+      System.out.println("\nLUNCH");
+      printMenu(dinerIterator);
     }
 
     private void printMenu(Iterator iterator) {
-        while (iterator.hasNext()) {
-            MenuItem menuItem = (MenuItem) iterator.next();
-            System.out.print(menuItem.getName() + ", ");
-            System.out.print(menuItem.getPrice() + " -- ");
-            System.out.println(menuItem.getDescription());
-        }
+      while (iterator.hasNext()) {
+        MenuItem menuItem = (MenuItem) iterator.next();
+        System.out.print(menuItem.getName() + ", ");
+        System.out.print(menuItem.getPrice() + " -- ");
+        System.out.println(menuItem.getDescription());
+      }
     }
 
 }
